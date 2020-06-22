d7a57e2dbbfcdb4d6b90fd6d233d8dec563cdb4d
Shatakshi Upadhyay <shatakshiupadhyay.19@gmail.com>
2020-06-23 00:08:26 +0530

    project done

---
+++ index.js
@@ -0,0 +1,104 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, activeplayer, roundscore, dice , gamePlaying ;
+
+init();
+//document.querySelector('.btn-roll').addEventListener('click',btn); //btn here is a callback function 
+// but we are going to use an anonymous funtion here so ..
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+//this is where we are writing an anonymous function    
+//steps 
+if(gamePlaying){
+dice = Math.floor(Math.random() *6) + 1;    // 1. Random number 
+console.log(dice);
+//2. Display the result
+var diceDOM = document.querySelector('.dice');
+diceDOM.style.display = 'block';
+diceDOM.src = 'DICE_'+ dice + '.png';
+
+//3. Update the value only if the value is NOT a 1
+if(dice!==1){
+    //Add the no.
+    roundscore += dice;
+    document.querySelector("#current-"+ activeplayer).textContent = roundscore;
+}
+else{
+    
+    nextPlayer();
+    roundscore=0;
+    // document.querySelector('.player-0').classList.remove('active');
+    // document.querySelector('.player-1').classList.add('active');
+    }
+}
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    if(gamePlaying){
+    scores[activeplayer]+= roundscore; // Add score 
+    //update the UI
+    document.querySelector('#score-'+ activeplayer).textContent = scores[activeplayer];
+    // check the winner
+    if(scores[activeplayer]>=100){
+        document.querySelector('#name-' + activeplayer ).textContent = 'WINNER!!!!'
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-1').classList.add('winner');
+        document.querySelector('.player-0').classList.remove('active');
+        gamePlaying= false;
+    }
+    else{
+        nextPlayer();
+        roundscore=0;
+        
+    }
+}
+
+});
+
+document.querySelector('.btn-new').addEventListener('click', init );// here we are not putting init() with paranthesis becoz then it will be invoked immediately but we dont want that we want to invoke this fuction when that event happen  
+
+
+function init(){
+    scores=[0,0];
+    roundscore=0;
+    activeplayer=0;
+    gamePlaying = true; 
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent='0';
+    document.getElementById('score-1').textContent='0';
+    document.getElementById('current-0').textContent='0';
+    document.getElementById('current-1').textContent='0';
+    document.getElementById('name-0').textContent='Player 1';
+    document.getElementById('name-1').textContent='Player 2';
+    document.querySelector('.player-0').classList.remove('winner');
+    document.querySelector('.player-1').classList.remove('winner');
+    document.querySelector('.player-0').classList.remove('active');
+    document.querySelector('.player-1').classList.remove('active');
+    document.querySelector('.player-0').classList.add('active');
+
+
+
+
+}
+
+function nextPlayer(){
+    activeplayer === 0 ? activeplayer = 1 : activeplayer = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0').classList.toggle('active');
+    document.querySelector('.player-1').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+}
+
